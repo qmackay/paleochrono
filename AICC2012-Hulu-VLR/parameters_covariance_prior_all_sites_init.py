b8bcc23fb45fa3fcef9e9ef196709b7a4a916994
Frédéric Parrenin <frederic.parrenin@univ-grenoble-alpes.fr>
2019-04-18 12:40:34 +0200

    Reformat AICC2012-Hulu parameter files.

--- AICC2012-Hulu-VLR/parameters_covariance_prior_all_sites_init.py
+++ AICC2012-Hulu-VLR/parameters_covariance_prior_all_sites_init.py
@@ -1,19 +1,23 @@
 ##In this file, the covariance matrix for the a prioris is defined.
-# At first, you will most likely play with the lambda_a, lambda_LID and lambda_tau parameters which are defined parameters-AllSites.py file.
+# At first, you will most likely play with the lambda_a, lambda_LID and lambda_tau parameters which
+#are defined parameters-AllSites.py file.
 
 #Accu correlation matrix
-self.correlation_corr_a=np.interp(np.abs(np.ones((np.size(self.corr_a_age),np.size(self.corr_a_age)))*self.corr_a_age-np.transpose(np.ones((np.size(self.corr_a_age),np.size(self.corr_a_age)))*self.corr_a_age)), np.array([0,self.lambda_a]),np.array([1, 0]))
-#Gaussian shape, does not work for a too high resolution
-#self.correlation_corr_a=gaussian(np.abs(np.ones((np.size(self.corr_a_age),np.size(self.corr_a_age)))*self.corr_a_age-np.transpose(np.ones((np.size(self.corr_a_age),np.size(self.corr_a_age)))*self.corr_a_age))/self.lambda_a)
+self.correlation_corr_a = np.interp(np.abs(np.ones((np.size(self.corr_a_age),\
+    np.size(self.corr_a_age)))*self.corr_a_age-np.transpose(np.ones((np.size(self.corr_a_age),\
+    np.size(self.corr_a_age)))*self.corr_a_age)), np.array([0,self.lambda_a]),np.array([1, 0]))
+
 
 if self.archive == 'icecore':
     #LID correlation matrix
-    self.correlation_corr_LID=np.interp(np.abs(np.ones((np.size(self.corr_LID_age),np.size(self.corr_LID_age)))*self.corr_LID_age-np.transpose(np.ones((np.size(self.corr_LID_age),np.size(self.corr_LID_age)))*self.corr_LID_age)), np.array([0,self.lambda_LID]),np.array([1, 0]) )
-    #Gaussian shape, does not work for a too high resolution
-    #self.correlation_corr_LID=gaussian(np.abs(np.ones((np.size(self.corr_LID_age),np.size(self.corr_LID_age)))*self.corr_LID_age-np.transpose(np.ones((np.size(self.corr_LID_age),np.size(self.corr_LID_age)))*self.corr_LID_age))/self.lambda_LID)
+    self.correlation_corr_LID = np.interp(np.abs(np.ones((np.size(self.corr_LID_age),\
+        np.size(self.corr_LID_age)))*self.corr_LID_age-np.transpose(np.ones((np.size(\
+        self.corr_LID_age),np.size(self.corr_LID_age)))*self.corr_LID_age)),\
+        np.array([0,self.lambda_LID]),np.array([1, 0]))
     
     #Thinning correlation matrix
-    self.correlation_corr_tau=np.interp(np.abs(np.ones((np.size(self.corr_tau_depth),np.size(self.corr_tau_depth)))*self.corr_tau_depth-np.transpose(np.ones((np.size(self.corr_tau_depth),np.size(self.corr_tau_depth)))*self.corr_tau_depth)), np.array([0,self.lambda_tau]),np.array([1, 0]) )
-    #Gaussian shape, does not work for a too high resolution
-    #correlation_corr_tau=gaussian(np.abs(np.ones((np.size(self.corr_tau_depth),np.size(self.corr_tau_depth)))*self.corr_tau_depth-np.transpose(np.ones((np.size(self.corr_tau_depth),np.size(self.corr_tau_depth)))*self.corr_tau_depth))/self.lambda_tau)
+    self.correlation_corr_tau = np.interp(np.abs(np.ones((np.size(self.corr_tau_depth),\
+        np.size(self.corr_tau_depth)))*self.corr_tau_depth-np.transpose(np.ones((np.size(\
+        self.corr_tau_depth),np.size(self.corr_tau_depth)))*self.corr_tau_depth)),\
+        np.array([0,self.lambda_tau]),np.array([1, 0]) )
     
