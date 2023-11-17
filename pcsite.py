Fix Jacobian for simple archives + 1:1 line label.

pcsite.py
@@ -741,21 +741,24 @@ class Site(object):
     def corrected_jacobian(self, full=False):
         """Calculate the Jacobian"""
 
-        if full:
-            self.accu_jac = np.zeros((1+len(self.corr_a)+len(self.corr_tau)+len(self.corr_lid), len(self.accu)))
-        self.age_jac = np.zeros((1+len(self.corr_a)+len(self.corr_tau)+len(self.corr_lid), len(self.age)))
-        self.age_jac[0, :] = self.age_top_sigma * np.ones(len(self.age))
         if self.archive == 'icecore':
+            self.age_jac = np.zeros((1+len(self.corr_a)+len(self.corr_tau)+len(self.corr_lid), len(self.age)))
             self.airage_jac = np.zeros((1+len(self.corr_a)+len(self.corr_tau)+len(self.corr_lid), len(self.airage)))
             self.airage_jac[0, :] = self.age_top_sigma * np.ones(len(self.airage))
             self.delta_depth_jac = np.zeros((1+len(self.corr_a)+len(self.corr_tau)+len(self.corr_lid), len(self.depth)))
     #        delta_depth_jac[0, :] = np.zeros(len(depth))
             if full:
-                self.icelayerthick_jac = np.zeros((1+len(self.corr_a)+len(self.corr_tau)+len(self.corr_lid), len(self.icelayerthick)))
-            #        icelayerthick_jac[0, :] = np.zeros(len(icelayerthick))
+                self.accu_jac = np.zeros((1+len(self.corr_a)+len(self.corr_tau)+len(self.corr_lid), len(self.accu)))
                 self.tau_jac = np.zeros((1+len(self.corr_a)+len(self.corr_tau)+len(self.corr_lid), len(self.tau)))
                 self.lid_jac = np.zeros((1+len(self.corr_a)+len(self.corr_tau)+len(self.corr_lid), len(self.lid)))
+                self.icelayerthick_jac = np.zeros((1+len(self.corr_a)+len(self.corr_tau)+len(self.corr_lid), len(self.icelayerthick)))
+            #        icelayerthick_jac[0, :] = np.zeros(len(icelayerthick))
+        else:
+            self.age_jac = np.zeros((1+len(self.corr_a), len(self.age)))
+            if full:
+                self.accu_jac = np.zeros((1+len(self.corr_a), len(self.accu)))
 
+        self.age_jac[0, :] = self.age_top_sigma * np.ones(len(self.age))
             
         for i in range(len(self.corr_a)):
 

pcsitepair.py
@@ -279,7 +279,7 @@ class SitePair(object):
 #            y_low = self.site2.age_top
 #            mpl.axis((x_low, x_up, y_low, y_up))
             rangefig = np.array([min(x_low, y_low), max(x_up, y_up)])
-            mpl.plot(rangefig, rangefig, color=pccfg.color_obs, label='Perfect agreement', zorder=0)
+            mpl.plot(rangefig, rangefig, color=pccfg.color_obs, label='1:1 line', zorder=0)
             mpl.legend(loc="best")
             ax.set_aspect('equal')
             printed_page = PdfPages(pccfg.datadir+self.label+'/'+self.age_age_label+'synchro.pdf')
@@ -326,7 +326,7 @@ class SitePair(object):
 #                y_low = self.site2.age_top
 #                mpl.axis((x_low, x_up, y_low, y_up))
                 rangefig = np.array([min(x_low, y_low), max(x_up, y_up)])
-                mpl.plot(rangefig, rangefig, color=pccfg.color_obs, label='Perfect agreement',
+                mpl.plot(rangefig, rangefig, color=pccfg.color_obs, label='1:1 line',
                          zorder=0)
                 mpl.legend(loc="best")
                 ax.set_aspect('equal')
@@ -375,7 +375,7 @@ class SitePair(object):
 #                y_low = self.site2.age_top
 #                mpl.axis((x_low, x_up, y_low, y_up))
                 rangefig = np.array([min(x_low, y_low), max(x_up, y_up)])
-                mpl.plot(rangefig, rangefig, color=pccfg.color_obs, label='Perfect agreement',
+                mpl.plot(rangefig, rangefig, color=pccfg.color_obs, label='1:1 line',
                          zorder=0)
                 mpl.legend(loc="best")
                 ax.set_aspect('equal')
@@ -423,7 +423,7 @@ class SitePair(object):
 #                y_low = self.site2.age_top
 #                mpl.axis((x_low, x_up, y_low, y_up))
                 rangefig = np.array([min(x_low, y_low), max(x_up, y_up)])
-                mpl.plot(rangefig, rangefig, color=pccfg.color_obs, label='Perfect agreement')
+                mpl.plot(rangefig, rangefig, color=pccfg.color_obs, label='1:1 line')
                 mpl.legend(loc="best")
                 ax.set_aspect('equal')
                 printed_page = PdfPages(pccfg.datadir+self.label+'/'+self.age2_age_label+